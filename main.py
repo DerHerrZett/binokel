620251a9e6e5f89a97b155b4ddb5c4bc24272afb
Daniel <daniel.x.zander@gmail.com>
2024-07-17 22:19:36 +0200

    messing

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from binokel import card
 
+cards = []
 for name in card.CardName:
     value = card.CardRank(name=name, value=0)
     for suit in card.CardSuit:
-        print(card.Card(suit=suit, rank=value).show())
+        current_card = card.Card(suit=suit, rank=value).show()
+        cards += [current_card,current_card]
+
+print(cards)
+print(len(cards))
 
